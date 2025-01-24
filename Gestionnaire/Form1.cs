d3fd339df26ca5d7355c2d5a0b27afcbc15d4ef9
ShanthakumarThanus <thanus.shanthakumar.sio@gmail.com>
2025-01-24 23:25:22 +0100

    first commit

---
+++ Gestionnaire/Form1.cs
@@ -0,0 +1,10 @@
+namespace Gestionnaire
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ database_test.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS gestionnaire;
+
+CREATE TABLE IF NOT EXISTS `gestionnaire` (
+    id_utilisateur INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nom VARCHAR (255),
+    prenom VARCHAR (255),
+    matricule VARCHAR (255),
+    poste VARCHAR (255)
+)
+
+INSERT INTO gestionnaire (id_utilisateur, nom, prenom, matricule, poste)
+VALUES (1, 'Test', 'Test', '111ZZZA', 'Test'); 
